d8254d9a7f870eae5d87e9cf7fc4fd40e81eb298
Samy Abdouche <samy.abdouche@gmail.com>
2020-06-02 14:02:38 +0200

    Fonctions d'extractions des triplets (f_fléchie, cat, lemme) avec streams

--- exercice2/print_stream.ml
+++ exercice2/print_stream.ml
@@ -18,21 +18,18 @@ let extract_line (line:string) : (string*string*string) = let l = String.split l
 let rec extract_rec ic  = let line = Stdio.In_channel.input_line ic in match line with
 	| Some _ -> let triplet = getOption line |> extract_line in triplet::(extract_rec ic)
 	| None -> []
-;;
 
 
 (* val extract : In_channel -> (string*string*string) stream *)
 let rec extract_stream ic = let line = Stdio.In_channel.input_line ic in match line with
 	| None -> Nil
 	| Some _ -> let triplet = getOption line |> extract_line in Cons(triplet, fun () -> extract_stream ic)
-;;
 	
 
 (* val iter_stream : 'a stream -> ('a -> unit) -> unit *)
 let rec iter_stream st ~f:f = match st with
 	| Nil -> ()
 	| Cons (e,q) -> f e; iter_stream (q ()) ~f:f
-;;
 
 
 
